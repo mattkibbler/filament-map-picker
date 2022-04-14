e60835c04277e29d497e9fc5a920393506bb8dd6
Matt Kibbler <mattkibbler@gmail.com>
2022-04-14 17:47:48 +0100

    Use default marker icon

--- resources/js/map-picker.js
+++ resources/js/map-picker.js
@@ -33,14 +33,7 @@ window.mapPicker = ($wire, config) => {
 
             if (config.showMarker === true) {
 				const markerOptions = config.markerOptions || {};
-                const svgIcon = L.divIcon({
-                    html: `<svg xmlns="http://www.w3.org/2000/svg" class="map-icon" width="36" height="36" viewBox="0 0 24 24"><path d="M12 0c-4.198 0-8 3.403-8 7.602 0 4.198 3.469 9.21 8 16.398 4.531-7.188 8-12.2 8-16.398 0-4.199-3.801-7.602-8-7.602zm0 11c-1.657 0-3-1.343-3-3s1.343-3 3-3 3 1.343 3 3-1.343 3-3 3z"/></svg>`,
-                    className: "",
-                    iconSize: [36, 36],
-                    iconAnchor: [18, 36],
-                });
                 this.marker = L.marker([0, 0], {
-                    icon: svgIcon,
                     draggable: false,
                     autoPan: true,
 					...markerOptions,
